11e072b5a42979cd3e2db5962e63a1b5c4046bfb
salgxv <salvarela09@yahoo.com>
2025-04-21 14:44:03 -0700

    pages

--- Develop/src/App.tsx
+++ Develop/src/App.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from 'react-router-dom';
-import Nav from './components/Nav';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CandidateSearch from "./pages/CandidateSearch";
+import SavedCandidates from "./pages/SavedCandidates";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <>
+    <Router>
       <Nav />
-      <main>
-        <Outlet />
-      </main>
-    </>
+      <Routes>
+        <Route path="/" element={<CandidateSearch />} />
+        <Route path="/saved" element={<SavedCandidates />} />
+      </Routes>
+    </Router>
   );
 }
 
