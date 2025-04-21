de4ea658892d8ad95bb9940e7da1217d235a358a
salgxv <salvarela09@yahoo.com>
2025-04-21 14:36:43 -0700

    asdfasdf

--- Develop/src/components/CandidateCard.tsx
+++ Develop/src/components/CandidateCard.tsx
@@ -3,8 +3,8 @@ import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
 
 interface CandidateCardProps {
   candidate: Candidate;
-  onAdd: () => void;
-  onIgnore: () => void;
+  onAdd?: () => void;       
+  onIgnore?: () => void;    
 }
 
 const CandidateCard: React.FC<CandidateCardProps> = ({ candidate, onAdd, onIgnore }) => {
@@ -17,10 +17,14 @@ const CandidateCard: React.FC<CandidateCardProps> = ({ candidate, onAdd, onIgnor
       <p>Location: {candidate.location}</p>
       <p>Email: <a href={`mailto:${candidate.email}`}>{candidate.email}</a></p>
       <p>Company: {candidate.company}</p>
-      <div className="candidate-actions">
-  {onIgnore && <FaMinusCircle className="redButton" onClick={onIgnore} />}
-  {onAdd && <FaPlusCircle className="greenButton" onClick={onAdd} />}
-</div>
+
+      {/* Only show buttons if functions are provided */}
+      {(onAdd || onIgnore) && (
+        <div className="candidate-actions">
+          {onIgnore && <FaMinusCircle className="redButton" onClick={onIgnore} />}
+          {onAdd && <FaPlusCircle className="greenButton" onClick={onAdd} />}
+        </div>
+      )}
     </div>
   );
 };
