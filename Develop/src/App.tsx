59e72bb79cdd92e7fde7a1cc17e9bea792231aa0
salgxv <salvarela09@yahoo.com>
2025-04-21 14:47:52 -0700

    idk

--- Develop/src/App.tsx
+++ Develop/src/App.tsx
@@ -1,17 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CandidateSearch from "./pages/CandidateSearch";
-import SavedCandidates from "./pages/SavedCandidates";
-import Nav from "./components/Nav";
+import { Outlet } from 'react-router-dom';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <Router>
+    <>
       <Nav />
-      <Routes>
-        <Route path="/" element={<CandidateSearch />} />
-        <Route path="/saved" element={<SavedCandidates />} />
-      </Routes>
-    </Router>
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
 
--- Develop/src/main.tsx
+++ Develop/src/main.tsx
@@ -1,31 +1,30 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-
-import App from './App.tsx';
-import CandidateSearch from './pages/CandidateSearch.tsx';
-import SavedCandidates from './pages/SavedCandidates.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import CandidateSearch from "./pages/CandidateSearch";
+import SavedCandidates from "./pages/SavedCandidates";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
-    errorElement: <ErrorPage />,
     children: [
       {
-        index: true,
+        path: "/",
         element: <CandidateSearch />,
       },
       {
-        path: '/SavedCandidates',
+        path: "/saved",
         element: <SavedCandidates />,
       },
     ],
   },
 ]);
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
